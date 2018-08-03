5903345dbf1f629c95498adbdb31e3c6556f00e9
Elaine Anderson <eparie@bu.edu>
2018-08-03 22:13:40 +0000

    automatically backed up by learn

--- db/migrate/20180803210842_create_join_table_taxis_passengers.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableTaxisPassengers < ActiveRecord::Migration
-  def change
-    create_join_table :taxis, :passengers do |t|
-       t.index :taxi_id
-       t.index :passenger_id
-    end
-  end
-end
